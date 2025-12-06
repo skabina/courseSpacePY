559d582f9baf0cd5254a87f768aaa1b5b2f8ea94
Borch Andrii <skabinausa@gmail.com>
2025-12-06 12:54:33 +0200

    Path and parameters

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world!"}
+@app.get("/items/{item_id}")
+async def root(item_id : int):
+    return {"item_id": item_id}
