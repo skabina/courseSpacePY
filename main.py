cf1342f5c6426c34cc5760af2525222a690ba227
Borch Andrii <skabinausa@gmail.com>
2025-12-09 21:50:37 +0200

    finish base Path FastApi

--- main.py
+++ main.py
@@ -4,26 +4,6 @@ from enum import Enum
 app = FastAPI()
 
 
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-    kokogambo = "kokogambo"
-
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name": {model_name}, "message": "Deep Learning FTW!"}
-    
-    if model_name.value == model_name.kokogambo.value:
-        return {"model_name": {model_name}, "message": "LEARN X in world"}
-    
-    if model_name.value ==  "resnet":
-        return {"model_name": {model_name}, "message": "LeCNN all the images"}
-    
-    return {"model_name": {model_name}, "message": "Have some residuals"}
-
-
+@app.get("/files/{file_path}")
+def read_file(file_path: str):
+    return {"file_path": file_path}
